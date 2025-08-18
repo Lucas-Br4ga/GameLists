7690d3b2bfba3d3c425dcdb579f6270b65489aa4
Lucas Braga <lucasrbraga77@gmail.com>
2025-08-18 16:26:34 -0300

    Project created

---
+++ src/main/java/com/Lucas_Br4ga/gameList/GameListApplication.java
@@ -0,0 +1,13 @@
+package com.Lucas_Br4ga.gameList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameListApplication.class, args);
+	}
+
+}
